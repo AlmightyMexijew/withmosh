0959c7dbbab0063d7d50a7de8c34f52470801f0a
Gershon <CTepper625@gmail.com>
2022-04-13 12:08:33 +0300

    index5,6

---
+++ index5.js
@@ -0,0 +1,7 @@
+let name = 'Mosh';
+let age = 30;
+let isApproved = false;
+let firstName = undefined;
+let selectedColor = null;
+
+// what happens is that we can typeof on any of these....so typeof name shows string. if name = 1, we typeof name again and it shows "number". This is because the type changed dynamically to fit the new assignment.
---
+++ index6.js
@@ -0,0 +1,5 @@
+let person = {
+    name: 'Gershon',
+    age: 30
+};
+//this helps us so we don't need name = 'Gershon' age = 30
