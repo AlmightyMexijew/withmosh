310e51d878c120951791c7fa9ce2b61f5c860e2a
Gershon <CTepper625@gmail.com>
2022-04-13 11:56:46 +0300

    index4.js

--- index3.js
+++ index3.js
@@ -2,3 +2,8 @@
 let interestRate = 0.3;
 interestRate = 1;
 console.log(interestRate);
+// the value of constant doesn't change
+
+const interestRate2 = 0.3;
+interestRate2 = 1;
+console.log(interestRate2);
---
+++ index4.js
@@ -0,0 +1,6 @@
+let name = 'Gershon'; // string literal....literally a string of text.
+let age = 35; // Number Literal
+let isApproved = true; // Boolean Literal   ....Value can shift true -> false or false -> true
+let firstName = undefined; // undefined...no value set..we don't have yet
+let lastName = null; // Where we explicitly want to clear the value.
+//when we change it, we assign new thing, and then reset to null when clearing.
